2f41018f5b7cc3ce29ae59030b11d15cab9c8c92
Ramin1001 <raminba@code.edu.az>
2021-05-16 19:31:59 +0400

    Start App, made model, Set Db(Migration),working on TransactionContoller and views

---
+++ ASP.NET_CORE_CRUD-Jquery_AJAX/ASP.NET_CORE_CRUD-Jquery_AJAX/Models/TransactionDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_CORE_CRUD_Jquery_AJAX.Models
+{
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> option):base(option)
+        {
+                
+        }
+
+        public DbSet<TransactionModel> Transactions { get; set; }
+
+
+
+    }
+}
